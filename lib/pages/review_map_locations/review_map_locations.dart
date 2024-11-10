b8e94e537ce7b276206f1667b87c9e40227464d0
Daniel Halwell <danielhalwell@gmail.com>
2024-11-10 17:04:16 +0000

    Added page stubs for the review list

---
+++ lib/pages/review_map_locations/review_map_locations.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ReviewMapLocations extends StatefulWidget {
+  const ReviewMapLocations({super.key});
+
+  static const String route = '/review_map_locations';
+
+  @override
+  State<ReviewMapLocations> createState() => _State();
+}
+
+class _State extends State<ReviewMapLocations> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
